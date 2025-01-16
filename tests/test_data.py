7af5d021beedf3da1f815ec3113c0e2aab493cf6
OliverBroager <101400037+OliverBroager@users.noreply.github.com>
2025-01-16 10:52:05 +0100

    Updates to test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,9 @@
 from torch.utils.data import Dataset
 
-from src.image_classifier.data import MyDataset
+from src.image_classifier.data import Datahandler
 
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
+    dataset = Datahandler('data/processed', 'data/raw')
     assert isinstance(dataset, Dataset)
