539f7dafac74b8885ccfdbcf7b61b683c0c364bf
Emilie Nilsson <s204204@student.dtu.dk>
2025-01-16 12:09:53 +0100

    configs and continuous integration

--- src/image_classifier/train.py
+++ src/image_classifier/train.py
@@ -2,19 +2,21 @@ import os
 
 import pytorch_lightning as pl
 import wandb
+import hydra
 
 from data import AnimalDataModule
 from model import ImageClassifier
 
-def main():
+@hydra.main(config_path='configs', config_name='train')
+def main(cfg):
     # TODO: hyperparameters from config file here
 
     model = ImageClassifier(num_classes=10)
 
     trainer = pl.Trainer(
-                        max_epochs=10,
-                        log_every_n_steps=10
-                        #logger=pl.loggers.WandbLogger(project=os.environ['WANDB_PROJECT'], entity=os.environ['WANDB_ENTITY'])
+                        max_epochs=cfg.hyperparameters.max_epochs,
+                        log_every_n_steps=cfg.hyperparameters.log_steps,
+                        logger=pl.loggers.WandbLogger(project=os.getenv('WANDB_PROJECT'), entity=os.getenv('WANDB_ENTITY'))
     )
 
     trainer.fit(model, 
