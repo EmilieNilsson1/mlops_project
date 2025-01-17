339ff6e1289975f060c3ace3c28a409b9bc57f20
Emilie Nilsson <s204204@student.dtu.dk>
2025-01-17 10:47:14 +0100

    command line and pre-commit

--- src/image_classifier/model.py
+++ src/image_classifier/model.py
@@ -7,10 +7,11 @@ class ImageClassifier(pl.LightningModule):
     """
     A simple image classifier using a pretrained ResNet18 model.
     """
-    def __init__(self, num_classes: int):
+    def __init__(self, num_classes: int, lr: float = 1e-3):
         super(ImageClassifier, self).__init__()
+        self.lr = lr
         self.model = timm.create_model('resnet18', pretrained=True)
-        
+
         # remove final layer and replace with linear layer with num_classes outputs
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
@@ -18,8 +19,8 @@ class ImageClassifier(pl.LightningModule):
 
     def forward(self, x: torch.Tensor):
         return self.model(x)
-    
-    def training_step(self, 
+
+    def training_step(self,
                       batch: tuple[torch.Tensor, torch.Tensor],
                       batch_idx: int):
         im, label = batch
@@ -29,7 +30,7 @@ class ImageClassifier(pl.LightningModule):
         self.log('train_loss', loss)
         self.log('train_acc', acc)
         return loss
-    
+
     def validation_step(self,
                         batch: tuple[torch.Tensor, torch.Tensor],
                         batch_idx: int):
@@ -40,6 +41,6 @@ class ImageClassifier(pl.LightningModule):
         self.log('val_loss', loss, on_epoch=True)
         self.log('val_acc', acc, on_epoch=True)
         return loss
-    
+
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
