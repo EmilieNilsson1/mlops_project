a452b4f1846f2c6175d80dcb3bb140a0a56a4da8
Emilie Nilsson <s204204@student.dtu.dk>
2025-01-21 09:35:40 +0100

    formatting and adding ruff

--- src/image_classifier/train.py
+++ src/image_classifier/train.py
@@ -2,7 +2,6 @@ import os
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
-import wandb
 import hydra
 from pathlib import Path
 
@@ -10,40 +9,35 @@ from image_classifier.data import AnimalDataModule
 from image_classifier.model import ImageClassifier
 
 
-@hydra.main(config_path='../../configs', config_name='train.yaml')
+@hydra.main(config_path="../../configs", config_name="train.yaml")
 def main(cfg) -> None:
-
     model = ImageClassifier(num_classes=10, lr=cfg.hyperparameters.lr)
-    
+
     cur = Path.cwd()
     parent_directory = cur.parent.parent.parent
 
-    checkpoint_callback = ModelCheckpoint(dirpath='./models', monitor="val_loss", mode="min")
+    checkpoint_callback = ModelCheckpoint(dirpath="./models", monitor="val_loss", mode="min")
 
     trainer = pl.Trainer(
-                        callbacks=[checkpoint_callback],
-                        max_epochs=cfg.hyperparameters.epochs,
-                        log_every_n_steps=cfg.hyperparameters.log_steps,
-                        logger=pl.loggers.WandbLogger(project=os.getenv('WANDB_PROJECT'), entity=os.getenv('WANDB_ENTITY'))
+        callbacks=[checkpoint_callback],
+        max_epochs=cfg.hyperparameters.epochs,
+        log_every_n_steps=cfg.hyperparameters.log_steps,
+        logger=pl.loggers.WandbLogger(project=os.getenv("WANDB_PROJECT"), entity=os.getenv("WANDB_ENTITY")),
     )
 
     # hydra changes working dir to outpurs, getting back to the root
 
-
     # Initializing the data module
     data_module = AnimalDataModule(
-        Path(str(parent_directory) + '/data/processed'),
-        Path(str(parent_directory) + '/data/processed/images'),
+        Path(str(parent_directory) + "/data/processed"),
+        Path(str(parent_directory) + "/data/processed/images"),
         batch_size=cfg.hyperparameters.batch_size,
         split_ratio=[0.8, 0.1, 0.1],  # 80% train, 10% val, 10% test
-        seed=cfg.hyperparameters.seed
+        seed=cfg.hyperparameters.seed,
     )
 
     # Training the model
-    trainer.fit(model,
-                data_module
-                )
-    
+    trainer.fit(model, data_module)
 
 
 if __name__ == "__main__":
--- tests/load_test.py
+++ tests/load_test.py
@@ -1,6 +1,6 @@
-import random
 from locust import HttpUser, between, task
 from pathlib import Path
+
 
 class MyUser(HttpUser):
     """A simple Locust user class that defines the tasks to be performed by the users."""
@@ -18,11 +18,8 @@ class MyUser(HttpUser):
         """A task that simulates a user uploading an image to the predict endpoint."""
         image_path = Path.cwd() / "data/processed/images/cane_OIP-_3acmW_iSr12XgQTNz0IdQHaFj.jpeg"
         with open(image_path, "rb") as image_file:
-            self.client.post(
-                "/predict/",
-                files={"file": image_file},
-                headers={"accept": "application/json"}
-            )
-            
+            self.client.post("/predict/", files={"file": image_file}, headers={"accept": "application/json"})
+
+
 # Run the Locust load test: uvicorn src.image_classifier.api:app --reload
 # In another terminal: locust -f tests/test_api.py --host=http://127.0.0.1:8000
