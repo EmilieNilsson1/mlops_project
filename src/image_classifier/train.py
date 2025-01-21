ac810a8b0d0f9a1d6a628cc38b12a8845f86e745
Emilie Nilsson <s204204@student.dtu.dk>
2025-01-21 09:56:27 +0100

    fixed merge bug

--- src/image_classifier/train.py
+++ src/image_classifier/train.py
@@ -18,13 +18,8 @@ def main(cfg) -> None:
     parent_directory = cur.parent.parent.parent
 
     checkpoint_callback = ModelCheckpoint(dirpath="./models", monitor="val_loss", mode="min")
-    checkpoint_callback = ModelCheckpoint(dirpath="./models", monitor="val_loss", mode="min")
 
     trainer = pl.Trainer(
-        callbacks=[checkpoint_callback],
-        max_epochs=cfg.hyperparameters.epochs,
-        log_every_n_steps=cfg.hyperparameters.log_steps,
-        logger=pl.loggers.WandbLogger(project=os.getenv("WANDB_PROJECT"), entity=os.getenv("WANDB_ENTITY")),
         callbacks=[checkpoint_callback],
         max_epochs=cfg.hyperparameters.epochs,
         log_every_n_steps=cfg.hyperparameters.log_steps,
@@ -35,15 +30,12 @@ def main(cfg) -> None:
     data_module = AnimalDataModule(
         Path(str(parent_directory) + "/data/processed"),
         Path(str(parent_directory) + "/data/processed/images"),
-        Path(str(parent_directory) + "/data/processed"),
-        Path(str(parent_directory) + "/data/processed/images"),
         batch_size=cfg.hyperparameters.batch_size,
         split_ratio=[0.8, 0.1, 0.1],  # 80% train, 10% val, 10% test
         seed=cfg.hyperparameters.seed,
     )
 
     # Training the model
-    trainer.fit(model, data_module)
     trainer.fit(model, data_module)
 
 
