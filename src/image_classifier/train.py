9597501edfd5be743ec9250e6dfa2174ebba101c
Viktor Marius Nysøe Rehling <s224235@dtu.dk>
2025-01-21 11:44:27 +0100

    update data folder paths for image classification training

--- src/image_classifier/train.py
+++ src/image_classifier/train.py
@@ -25,11 +25,18 @@ def main(cfg) -> None:
         log_every_n_steps=cfg.hyperparameters.log_steps,
         logger=pl.loggers.WandbLogger(project=os.getenv("WANDB_PROJECT"), entity=os.getenv("WANDB_ENTITY")),
     )
+    
+    if os.path.exists('/gcs/mlops_project25_group72/data/p'):
+        label_folder = '/gcs/mlops_project25_group72/data/p'
+        image_folder = '/gcs/mlops_project25_group72/data/p/images'
+    else:
+        image_folder = str(parent_directory) + '/data/processed/images'
+        label_folder = str(parent_directory) + '/data/processed
 
     # Initializing the data module
     data_module = AnimalDataModule(
-        Path(str(parent_directory) + "/data/processed"),
-        Path(str(parent_directory) + "/data/processed/images"),
+        Path(label_folder),
+        Path(image_folder),
         batch_size=cfg.hyperparameters.batch_size,
         split_ratio=[0.8, 0.1, 0.1],  # 80% train, 10% val, 10% test
         seed=cfg.hyperparameters.seed,
